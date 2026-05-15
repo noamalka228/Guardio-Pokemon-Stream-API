83fccfc575457b4ac1b1d354ce4865ee09b82144
Noam Malka <noamalka2903@gmail.com>
2026-05-15 16:37:44 +0300

    Added /stream route

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,8 @@
 """
 Router for defining API routes.
 """
-from fastapi import APIRouter
+from json import JSONDecodeError
+from fastapi import APIRouter, Request
 
 router = APIRouter()
 
@@ -12,3 +13,14 @@ def read_root():
     Returns a welcome message.
     """
     return {"message": "Welcome to Pokemon Stream API"}
+
+@router.post("/stream", tags=["Pokemon"])
+async def stream(request: Request):
+    """
+    Returns a stream of pokemon.
+    """
+    try:
+        data = await request.json()
+    except JSONDecodeError:
+        return {"message": "Invalid JSON"}
+    return {"message": data}
