4c843b6b1ccd1de21dc8eb4523f430fd406290b9
Noam Malka <noamalka2903@gmail.com>
2026-05-19 00:54:22 +0300

    Replaced initialization as object with @property and @computed_field

--- services/proxy/app/core/config.py
+++ services/proxy/app/core/config.py
@@ -2,6 +2,7 @@
 Application configuration.
 """
 import base64
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -12,13 +13,12 @@ class Settings(BaseSettings):
     app_name: str = "Guardio Pokemon Stream API"
     stream_secret: str = ""
     pokeproxy_config: str = ""
-    decoded_secret_bytes: bytes = b""
 
-    # TODO: replace this __init__ block with cleaner code
-    def __init__(self, **values):
-        super().__init__(**values)
+    @computed_field
+    @property
+    def decoded_secret_bytes(self) -> bytes:
         try:
-            self.decoded_secret_bytes = base64.b64decode(self.stream_secret)
+            return base64.b64decode(self.stream_secret)
         except Exception:
             raise ValueError("Invalid stream secret format")
 
