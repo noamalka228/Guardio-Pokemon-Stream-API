177a81b62577ea9f5c3c3356772728ac10247772
Noam Malka <noamalka2903@gmail.com>
2026-05-18 11:08:02 +0300

    Change d to absolute import

--- services/proxy/app/main.py
+++ services/proxy/app/main.py
@@ -2,7 +2,7 @@
 Main entry point for the Pokemon stream API.
 """
 from fastapi import FastAPI
-from .api.routes import router
+from app.api.routes import router
 
 app = FastAPI(title="Pokemon Stream API")
 app.include_router(router)
